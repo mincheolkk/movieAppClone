1be1ac6caa654cad69367956b1b4c73d896520ba
mincheolkk <kmc04080408@gmail.com>
2022-03-05 23:35:36 +0900

    컴포넌트 학습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <h1>h1</h1>
+      hello
+      <Food fav="kimchi" something={true} papa={['hello',1,2,3, true]} />
+      <Food fav="깍뚜기" />
+
     </div>
   );
 }
 
+function Food(props){
+  console.log(props);
+  return <h1>Potato Love2 {props.fav} </h1>;
+}
 export default App;
