781a1c015388b3fc9cd465c982ad045c6dfffb16
mincheolkk <kmc04080408@gmail.com>
2022-03-12 01:03:18 +0900

    함수 실행 순서 확인

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
 import React from "react";
 
-const foodList = [
-  {
-    name : "kimchi",
-    image : "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjAxMjZfMjY4%2FMDAxNjQzMTczNjIyMDcx.rmnlGq-Uta7VR7wFuVFom3bCtVi9eKawHt1pGLI1PjIg.aUX1g3lg8xCE9Yqbn3jC-YlsM14opEJ_mPvaTxD97j4g.JPEG.minamkkjs%2F%25BA%25ED%25B7%25CE%25B1%25D71.jpg&type=sc960_832",
-  },
-  {
-    name : "pizza",
-    image : "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fmedia-cdn.tripadvisor.com%2Fmedia%2Fphoto-p%2F14%2Ff8%2Fc2%2F9d%2Fpizza.jpg&type=sc960_832"
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log('hello, i am constructor !');
   }
-]
 
+  state = {
+    count: 0,
+  };
 
-function App() {
-  console.log(foodList.map(renderFood))
+  add = () => {
+    // console.log('add');
+    this.setState(x => ({
+      count: x.count + 3,
+    }));
 
-  return (
-    <div> 
-      <h1>h1</h1>
-      hello
-      <Food fav="22" some="44"/>
-      <Food fav="33"/>
-      {foodList.map(renderFood)}
+  };
+
+  minus = () => {
+    console.log('minus');
+  };
+
+  componentDidMount() {
+    console.log('hi i am cdm');
+  }
+
+  componentDidUpdate() {
+    console.log('oh, i m update');
+  }
+
+  render() {
+    console.log('render ! ');
+    return (
+    <div>
+      <h1> count is : {this.state.count} </h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>minus</button>
     </div>
-  );
-}
-
-function Food({name, pic}){
-  return (
-        <div><h1>{name}</h1>
-        <img src={pic} /></div>);
-}
-
-function renderFood(d) {
-  return <Food name={d.name} pic={d.image} />;
+    );
+  }
 }
 
 export default App;
